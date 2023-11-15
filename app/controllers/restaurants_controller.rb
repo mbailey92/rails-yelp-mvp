0213b35dfa36fd620416c805961200aa72c6bd79
Michael Bailey <michaelbailey92@gmail.com>
2023-11-15 16:43:18 +0000

    rails yelp done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,5 +5,10 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
   end
 end

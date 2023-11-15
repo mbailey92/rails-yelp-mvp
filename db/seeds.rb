81d73344a4ba0ef9de82b3826f462d8c99084839
Michael Bailey <michaelbailey92@gmail.com>
2023-11-15 14:02:38 +0000

    simple form done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Creating restaurants...'
+le_gavroche = { name: 'Le Gavroche', address: '43 Upper Brook St, London W1K 7QR', phone_number: '02074080881', category: 'french' }
+locanda_locatelli =  { name: 'Locanda Locatelli', address: '8 Seymour St, London W1H 7JZ', phone_number: '02079359088', category: 'italian' }
+lowlander = { name: 'Lowlander', address: '36 Drury Ln, London WC2B 5RR', phone_number: '02073797446', category: 'belgian' }
+the_araki = { name: 'The Araki', address: '12 New Burlington St, London W1S 3BF', phone_number: '02072872481', category: 'japanese' }
+hakkasan = { name: 'Hakkasan', address: '17 Bruton St, London W1J 6QB', phone_number: '02079071888', category: 'chinese' }
+
+[le_gavroche, locanda_locatelli, lowlander, the_araki, hakkasan].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
